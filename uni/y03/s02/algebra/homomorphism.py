66fed080206fa7228bdd95b52fc7454561f2e663
Alex Holder <s2031787@ed.ac.uk>
2023-03-10 02:04:19 +0000

    algebra/homomorphism.py: add header

--- uni/y03/s02/algebra/homomorphism.py
+++ uni/y03/s02/algebra/homomorphism.py
@@ -1,4 +1,6 @@
-# Gets all values that g(1) can take, for g: Z/pZ -> Z/qZ
+# File: homomorphism.py 
+# date : 2023-03-10 
+# Description :  Gets all values that g(1) can take, for g: Z/pZ -> Z/qZ
 p = 6
 q = 66
 print([ x for x in range(q) if p * x % q == 0 and x == x**2 % q])
